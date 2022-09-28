cc19bc3a954e7d55b6d4ac0139a18e9301ea5248
kostochka120 <60574340+kostochka120@users.noreply.github.com>
2022-09-28 09:57:57 +0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,17 @@ module.exports = {
     module:{
         rules:[
             {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]'
+                }
+            },
+            {
                 test: /\.s(a|c)ss$/,
                 use: [            
                     miniCss.loader,
